df229dbee479ea7d1f069d61ec9a9ad0db56e49a
GitHub Action <action@github.com>
2025-09-15 11:35:46 +0000

    Bump version: 1.1.5 → 1.2.0

--- dialogue-guardian/setup.py
+++ dialogue-guardian/setup.py
@@ -20,7 +20,7 @@ if requirements_file.exists():
 
 setup(
     name="dialogue-guardian",
-    version="1.1.5",
+    version="1.2.0",
     author="Tony Snearly",
     description="Universal Media Censor - Automatically detect and censor profane language in video files",
     long_description=long_description,
