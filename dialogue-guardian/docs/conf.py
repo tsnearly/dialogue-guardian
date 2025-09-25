6de650bede77ecba6d58ce9dcc756581c7677378
Tony Snearly <tony.snearly@gmail.com>
2025-09-25 11:20:48 -0500

    Fix versioning where Geminii trashed the code

--- dialogue-guardian/docs/conf.py
+++ dialogue-guardian/docs/conf.py
@@ -15,20 +15,9 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'Dialogue Guardian'
 copyright = '2025, Tony Snearly'
 author = 'Tony Snearly'
-release = '1.2.0
+release = '1.2.0'
 version="{current_version}"
-version
-__version__'
-version="{current_version}"
-version
-__version__'
-version = '1.2.0
-version="{current_version}"
-version
-__version__'
-version="{current_version}"
-version
-__version__'
+current_version = '1.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
